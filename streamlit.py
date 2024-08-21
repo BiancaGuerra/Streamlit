cb2cb9cf9280827dcda3711a360a6db46c1bc86c
BiancaGuerra <bibi.r.guerra@hotmail.com>
2024-08-21 17:25:40 -0300

    Adicionei a função de listar as notas disponíveis

--- streamlit.py
+++ streamlit.py
@@ -1,24 +1,39 @@
 import streamlit as st
 import time
+import os
+
+def notas_disponiveis():
+    arquivos = []
+    notas = []
+    for arquivo in os.listdir(r'Y:\Publico\PS-SCM-COUNTRY LOGISTICS\Tableau\Base Consumos'):
+        if arquivo.endswith('.xlsx'):
+            arquivos.append(arquivo)
+    for nota in arquivos:
+        nota = nota[:-5]
+        notas.append(nota)
+    return notas
 
 def main():
-    st.title('TÍTULO')
-    st.write('texto')
+    notas = notas_disponiveis()
+    nota = st.selectbox('Selecione a nota fiscal que deseja cadastrar:', notas, index=None, placeholder="Escolha uma opção")
 
-    input = st.text_input('digite')
-    selecionada = st.selectbox('selecione', ['1', '2'])
-    slider = st.slider('escolha', 0, 100, 10)
-    checkbox = st.checkbox('marque')
-    if st.button('clique'):
-        st.write('vc apertou o botao')
-    with st.spinner('aguarde...'):
-        time.sleep(3)
-    st.success('pronto')
-    upload = st.file_uploader('escolha', type=['pdf'])
-    if upload:
-        st.write(f'vc fez o upload de: {upload.name}')
-    data = {'x': [1,2,3,4], 'y': [10,20,30,40]}
-    st.line_chart(data)
 
-    st.write('atualizadooo')
+    # st.title('TÍTULO')
+    # st.write('texto')
+
+    # input = st.text_input('digite')
+    # selecionada = st.selectbox('selecione', ['1', '2'])
+    # slider = st.slider('escolha', 0, 100, 10)
+    # checkbox = st.checkbox('marque')
+    # if st.button('clique'):
+    #     st.write('vc apertou o botao')
+    # with st.spinner('aguarde...'):
+    #     time.sleep(3)
+    # st.success('pronto')
+    # upload = st.file_uploader('escolha', type=['pdf'])
+    # if upload:
+    #     st.write(f'vc fez o upload de: {upload.name}')
+    # data = {'x': [1,2,3,4], 'y': [10,20,30,40]}
+    # st.line_chart(data)
+
 main()
